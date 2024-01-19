a9c1ed7aad3c39a45ec2589a33b34361fac7b227
ayaarragab <ayarsk4343@gmail.com>
2024-01-19 18:06:24 +0200

    deveops 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
+#!/usr/bin/pup
 # install flask from pip3.
+package { 'python3-pip':
+  ensure   => 'installed',
+}
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip],
+}
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
