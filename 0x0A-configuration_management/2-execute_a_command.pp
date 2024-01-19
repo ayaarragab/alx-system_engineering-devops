6fb9b259109ec6f5b5df228487531d830a59e827
ayaarragab <ayarsk4343@gmail.com>
2024-01-19 17:53:26 +0200

    deveops 3

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# i am forced to write a comment
-exec { 'killmenow_cod':
-    command => '/usr/bin/pkill killmenow',  # Adjust the path if needed
-    path => '.',
+# execute pkill command to kill bashscript file killmenow
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
