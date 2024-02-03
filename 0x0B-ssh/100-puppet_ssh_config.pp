29d8dd519b59132f695bea3c40eeb323d264bbc3
ayaarragab <ayarsk4343@gmail.com>
2024-02-03 17:15:40 +0200

    ssh config2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,4 @@
 # Replace IdentityFile in /root/.ssh/config
-exec { 'ssh_ident':
-  command => 'sed "s|IdentityFile *|IdentityFile ~/.ssh/school|" -i /root/.ssh/config',
-}
-
-# Disable PasswordAuthentication in /root/.ssh/config
-exec { 'ssh_password_auth':
-  command => 'sed "s|PasswordAuthentication *|PasswordAuthentication no|" -i /root/.ssh/config',
+exec { 'ssh_config':
+  command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
