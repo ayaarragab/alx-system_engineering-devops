a61b05b54add94384f5b0f58efdad8804e3472a5
ayaarragab <ayarsk4343@gmail.com>
2024-03-09 23:05:37 +0200

    1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,14 @@ def number_of_subscribers(subreddit):
     """
     return number_of_subscribers
     """
-    headers = {'User-agent': ''.join(['Mozilla/5.0',
-                                      '(Windows NT 10.0; Win64; x64)',
-                                      'AppleWebKit/537.36 (KHTML, like Gecko)',
-                                      'Chrome/58.0.3029.110',
-                                      'Safari/537.3'])}
+    headers = {'Accept': 'application/json',
+               'User-Agent': ' '.join([
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+                   'AppleWebKit/537.36 (KHTML, like Gecko)',
+                   'Chrome/97.0.4692.71',
+                   'Safari/537.36',
+                   'Edg/97.0.1072.62'
+               ])}
     url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
