c2178ce5322e793419eae463d480ba9d25c1be43
ayaarragab <ayarsk4343@gmail.com>
2024-05-10 16:35:58 +0300

    advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,8 @@ def number_of_subscribers(subreddit):
     """
     return number_of_subscribers
     """
-    headers = {'Accept': 'application/json',
-               'User-Agent': ' '.join([
-                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-                   'AppleWebKit/537.36 (KHTML, like Gecko)',
-                   'Chrome/97.0.4692.71',
-                   'Safari/537.36',
-                   'Edg/97.0.1072.62'
-               ])}
+    headers = {'User-Agent': "My-User-Agent"}
+
     url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
