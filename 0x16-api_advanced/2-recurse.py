1f180c67717eba32cdf635c02ad8a4ff0c316311
ayaarragab <ayarsk4343@gmail.com>
2024-05-10 16:43:33 +0300

    advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """
-a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit, the function should return None.
+a recursive function that queries the
+Reddit API and returns a list containing the titles
+of all hot articles for a given subreddit.
+If no results are found for the given
+subreddit, the function should return None.
 """
 
 
 def recurse(subreddit, hot_list=[], after=None):
     """
-    a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit, the function should return None.
+    a recursive function that queries the
+    Reddit API and returns a list containing the titles
+    of all hot articles for a given subreddit.
+    If no results are found for the given
+    subreddit, the function should return None.
     """
     import requests
 
